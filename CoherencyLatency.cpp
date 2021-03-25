6d810424ee432b577a12ebf4d5933aea4f52d532
Chester Lam <chelam@microsoft.com>
2021-03-25 13:23:24 -0700

    separate cache line test

--- CoherencyLatency.cpp
+++ CoherencyLatency.cpp
@@ -7,12 +7,15 @@
 #define ITERATIONS 10000000;
 
 float RunTest(unsigned int processor1, unsigned int processor2, uint64_t iter);
+float RunOwnedTest(unsigned int processor1, unsigned int processor2, uint64_t iter);
 DWORD WINAPI LatencyTestThread(LPVOID param);
+DWORD WINAPI ReadLatencyTestThread(LPVOID param);
 
 typedef struct LatencyThreadData {
     uint64_t start;       // initial value to write into target
     uint64_t iterations;  // number of iterations to run
     LONG64 *target;       // value to bounce between threads, init with start - 1
+    LONG64 *readTarget;   // for read test, memory location to read from (owned by other core)
     DWORD affinityMask;   // thread affinity mask to set
 } LatencyData;
 
@@ -21,6 +24,7 @@ int main(int argc, char *argv[]) {
     DWORD numProcs;
     float* latencies;
     uint64_t iter = ITERATIONS;
+    float (*test)(unsigned int, unsigned int, uint64_t) = RunTest;
 
     GetSystemInfo(&sysInfo);
     numProcs = sysInfo.dwNumberOfProcessors;
@@ -36,14 +40,21 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "%lu iterations requested\n", iter);
     }
     else {
-        fprintf(stderr, "Usage: coherencylatency.exe [iterations]\n");
+        fprintf(stderr, "Usage: coherencylatency.exe [iterations] [bounce/owned]\n");
+    }
+
+    if (argc > 2) {
+        if (strncmp(argv[2], "owned", 5) == 0) {
+            test = RunOwnedTest;
+            fprintf(stderr, "Using separate cache lines for each thread to write to\n");
+        }
     }
 
     // Run all to all, skipping testing a core against itself ofc
     // technically can skip the other way around (start j = i + 1) but meh
     for (DWORD i = 0; i < numProcs; i++) {
         for (DWORD j = 0; j < numProcs; j++) {
-            latencies[j + i * numProcs] = i == j ? 0 : RunTest(i, j, iter);
+            latencies[j + i * numProcs] = i == j ? 0 : test(i, j, iter);
         }
     }
 
@@ -52,10 +63,7 @@ int main(int argc, char *argv[]) {
         for (DWORD j = 0; j < numProcs; j++) {
             if (j != 0) printf(",");
             if (j == i) printf("x");
-            // other latency test (run by AT) likely divides by 2
-            // I personally disagree with this because writing to a line modified by another core
-            // involves an unavoidable round trip (request line from other core and receive the response)
-            else printf("%f", latencies[j + i * numProcs] / 2);
+            else printf("%f", latencies[j + i * numProcs]);
         }
         printf("\n");
     }
@@ -64,36 +72,13 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-/// <summary>
-/// Measures latency from one processor core to another
-/// </summary>
-/// <param name="processor1">processor number 1</param>
-/// <param name="processor2">processor number 2</param>
-/// <param name="iter">Number of iterations</param>
-/// <returns>latency per iteration in ns</returns>
-float RunTest(unsigned int processor1, unsigned int processor2, uint64_t iter)
-{
-    LONG64* bouncy;
+float TimeThreads(unsigned int processor1, unsigned int processor2, uint64_t iter, LatencyData lat1, LatencyData lat2, DWORD (*threadFunc)(LPVOID)) {
     struct timeb start, end;
     HANDLE testThreads[2];
     DWORD tid1, tid2;
-    LatencyData lat1, lat2;
 
-    bouncy = (LONG64*)_aligned_malloc(sizeof(LONG64), sizeof(LONG64));
-    if (bouncy == NULL) {
-        fprintf(stderr, "Could not allocate aligned mem\n");
-    }
-
-    *bouncy = 0;
-    lat1.iterations = iter;
-    lat1.start = 1;
-    lat1.target = bouncy;
-    lat2.iterations = iter;
-    lat2.start = 2;
-    lat2.target = bouncy;
-
-    testThreads[0] = CreateThread(NULL, 0, LatencyTestThread, &lat1, CREATE_SUSPENDED, &tid1);
-    testThreads[1] = CreateThread(NULL, 0, LatencyTestThread, &lat2, CREATE_SUSPENDED, &tid2);
+    testThreads[0] = CreateThread(NULL, 0, threadFunc, &lat1, CREATE_SUSPENDED, &tid1);
+    testThreads[1] = CreateThread(NULL, 0, threadFunc, &lat2, CREATE_SUSPENDED, &tid2);
 
     if (testThreads[0] == NULL || testThreads[1] == NULL) {
         fprintf(stderr, "Failed to create test threads\n");
@@ -116,13 +101,73 @@ float RunTest(unsigned int processor1, unsigned int processor2, uint64_t iter)
 
     CloseHandle(testThreads[0]);
     CloseHandle(testThreads[1]);
-    _aligned_free(bouncy);
 
+    // each thread does interlocked compare and exchange iterations times. divide by 2 to get overall count of locked ops
+    return latency / 2;
+}
+
+/// <summary>
+/// Measures latency from one processor core to another
+/// </summary>
+/// <param name="processor1">processor number 1</param>
+/// <param name="processor2">processor number 2</param>
+/// <param name="iter">Number of iterations</param>
+/// <param name="bouncy">aligned mem to bounce around</param>
+/// <returns>latency per iteration in ns</returns>
+float RunTest(unsigned int processor1, unsigned int processor2, uint64_t iter) {
+    LatencyData lat1, lat2;
+    LONG64* bouncy;
+    float latency;
+
+    bouncy = (LONG64*)_aligned_malloc(sizeof(LONG64), sizeof(LONG64));
+    if (bouncy == NULL) {
+        fprintf(stderr, "Could not allocate aligned mem\n");
+    }
+
+    *bouncy = 0;
+    lat1.iterations = iter;
+    lat1.start = 1;
+    lat1.target = bouncy;
+    lat2.iterations = iter;
+    lat2.start = 2;
+    lat2.target = bouncy;
+
+    latency = TimeThreads(processor1, processor2, iter, lat1, lat2, LatencyTestThread);
+    _aligned_free(bouncy);
+    return latency;
+}
+
+float RunOwnedTest(unsigned int processor1, unsigned int processor2, uint64_t iter) {
+    LatencyData lat1, lat2;
+    LONG64* target1, * target2;
+    float latency;
+
+    // drop them on different cache lines
+    target1 = (LONG64*)_aligned_malloc(128, 64);
+    target2 = target1 + 8;
+    if (target1 == NULL) {
+        fprintf(stderr, "Could not allocate aligned mem\n");
+    }
+
+    *target1 = 1;
+    *target2 = 0;
+    lat1.iterations = iter;
+    lat1.start = 3;
+    lat1.target = target1;
+    lat1.readTarget = target2;
+    lat2.iterations = iter;
+    lat2.start = 2;
+    lat2.target = target2;
+    lat2.readTarget = target1;
+
+    latency = TimeThreads(processor1, processor2, iter, lat1, lat2, ReadLatencyTestThread);
+    _aligned_free(target1);
     return latency;
 }
 
 /// <summary>
 /// Runs one thread of the latency test. should be run in pairs
+/// Always writes to target
 /// </summary>
 /// <param name="param">Latency test params</param>
 /// <returns>next value that would have been written to shared memory</returns>
@@ -130,7 +175,30 @@ DWORD WINAPI LatencyTestThread(LPVOID param) {
     LatencyData *latencyData = (LatencyData *)param;
     uint64_t current = latencyData->start;
     while (current <= 2 * latencyData->iterations) {
-        if (_InterlockedCompareExchange64(latencyData->target, current, current - 1) == current - 1) current += 2;
+        if (_InterlockedCompareExchange64(latencyData->target, current, current - 1) == current - 1) {
+            current += 2;
+        }
+    }
+
+    return current;
+}
+
+/// <summary>
+/// Similar thing but tries to not bounce cache line ownership
+/// Instead, threads write to different cache lines
+/// </summary>
+/// <param name="param">Latency test params</param>
+/// <returns>next value that would have been written to owned mem</returns>
+DWORD WINAPI ReadLatencyTestThread(LPVOID param) {
+    LatencyData* latencyData = (LatencyData*)param;
+    uint64_t current = latencyData->start;
+    uint64_t startTsc = __rdtsc();
+    while (current <= 2 * latencyData->iterations) {
+        if (*(latencyData->readTarget) == current - 1) {
+            *(latencyData->target) = current;
+            current += 2;
+            _mm_sfence();
+        }
     }
 
     return current;
