12c6a252746ab81ba577ea54ebb01060e8caf6a3
clamchowder <lamchester@gmail.com>
2021-10-01 22:53:01 -0700

    try private data per thread

--- MemoryBandwidth/MemoryBandwidth.c
+++ MemoryBandwidth/MemoryBandwidth.c
@@ -10,6 +10,7 @@
 #include <unistd.h>
 #include <sched.h>
 #include <pthread.h>
+#include <math.h>
 
 int default_test_sizes[39] = { 2, 4, 8, 12, 16, 24, 32, 48, 64, 96, 128, 192, 256, 512, 600, 768, 1024, 1536, 2048,
                                3072, 4096, 5120, 6144, 8192, 10240, 12288, 16384, 24567, 32768, 65536, 98304,
@@ -24,7 +25,7 @@ typedef struct BandwidthTestThreadData {
 } BandwidthTestThreadData;
 
 float scalar_read(float* arr, uint64_t arr_length, uint64_t iterations, uint64_t start);
-float MeasureBw(uint64_t sizeKb, uint64_t iterations, uint64_t threads);
+float MeasureBw(uint64_t sizeKb, uint64_t iterations, uint64_t threads, int shared);
 
 
 extern float asm_read(float* arr, uint64_t arr_length, uint64_t iterations, uint64_t start);
@@ -38,18 +39,33 @@ float (*bw_func)(float*, uint64_t, uint64_t, uint64_t start) = scalar_read;
 int main(int argc, char *argv[]) {
     int threads = 1;
     int cpuid_data[4];
+    int shared = 1;
+
     if (argc > 1) {
         threads = atoi(argv[1]);
+    }
+    if (argc > 2)
+    {
+        if (strncmp(argv[2], "private", 7) == 0)
+        {
+            shared = 0;
+            printf("Using private data per-thread\n");
+        }
+    }
+    
+    if (argc == 1)
+    {
+        printf("Usage: [threads] [shared/private]\n");
     }
 
     //bw_func = scalar_read;
     bw_func = asm_read;
-    printf("Using avx\n");
+    printf("Using asm\n");
 
     printf("Using %d threads\n", threads);
     for (int i = 0; i < sizeof(default_test_sizes) / sizeof(int); i++)
     {
-        printf("%d,%f\n", default_test_sizes[i], MeasureBw(default_test_sizes[i], GetIterationCount(default_test_sizes[i], threads), threads));
+        printf("%d,%f\n", default_test_sizes[i], MeasureBw(default_test_sizes[i], GetIterationCount(default_test_sizes[i], threads), threads, shared));
     }
 
     return 0;
@@ -72,21 +88,34 @@ uint64_t GetIterationCount(uint64_t testSize, uint64_t threads)
     else return iterations;
 }
 
-float MeasureBw(uint64_t sizeKb, uint64_t iterations, uint64_t threads) {
+float MeasureBw(uint64_t sizeKb, uint64_t iterations, uint64_t threads, int shared) {
     struct timeval startTv, endTv;
     struct timezone startTz, endTz;
     float bw = 0;
     uint64_t elements = sizeKb * 1024 / sizeof(float);
 
-    // make array and fill it with something
-    float* testArr = (float*)aligned_alloc(32, elements * sizeof(float));
-    if (testArr == NULL) {
-        fprintf(stderr, "Could not allocate memory\n");
-        return 0;
-    }
+    // make sure this is divisble by 512 bytes, since the unrolled asm loop depends on that
+    // it's hard enough to get close to theoretical L1D BW as is, so we don't want additional cmovs or branches
+    // in the hot loop
+    uint64_t private_elements = (uint64_t)ceil(((double)sizeKb * 1024 / sizeof(float)) / (double)threads);
+    //fprintf(stderr, "Actual data: %lu KB\n", private_elements * 4 * threads / 1024);
 
-    for (uint64_t i = 0; i < elements; i++) {
-        testArr[i] = i + 0.5f;
+    // make array and fill it with something, if shared
+    float* testArr = NULL;
+    if (shared){ 
+        testArr = (float*)aligned_alloc(32, elements * sizeof(float));
+        if (testArr == NULL) {
+            fprintf(stderr, "Could not allocate memory\n");
+            return 0;
+        }
+
+        for (uint64_t i = 0; i < elements; i++) {
+            testArr[i] = i + 0.5f;
+        }
+    }
+    else 
+    {
+        elements = private_elements; // will fill arrays below, per-thread
     }
 
     pthread_t* testThreads = (pthread_t*)malloc(threads * sizeof(pthread_t));
@@ -94,9 +123,28 @@ float MeasureBw(uint64_t sizeKb, uint64_t iterations, uint64_t threads) {
 
     gettimeofday(&startTv, &startTz);
     for (uint64_t i = 0; i < threads; i++) {
-        threadData[i].arr = testArr;
+        if (shared) 
+        {
+            threadData[i].arr = testArr;
+            threadData[i].iterations = iterations;
+        }
+        else
+        {
+            threadData[i].arr = (float*)aligned_alloc(32, elements * sizeof(float));
+            if (threadData[i].arr == NULL)
+            {
+                fprintf(stderr, "Could not allocate memory for thread %ld\n", i);
+                return 0;
+            }
+
+            for (uint64_t arr_idx = 0; arr_idx < elements; arr_idx++) {
+                threadData[i].arr[arr_idx] = arr_idx + i + 0.5f;
+            }
+
+            threadData[i].iterations = iterations * threads;
+        }
+
         threadData[i].arr_length = elements;
-        threadData[i].iterations = iterations;
         threadData[i].bw = 0;
         threadData[i].start = 0;
         if (elements > 8192 * 1024) threadData[i].start = 4096 * i; // must be multiple of 128 because of unrolling
@@ -109,10 +157,19 @@ float MeasureBw(uint64_t sizeKb, uint64_t iterations, uint64_t threads) {
     uint64_t time_diff_ms = 1000 * (endTv.tv_sec - startTv.tv_sec) + ((endTv.tv_usec - startTv.tv_usec) / 1000);
     double gbTransferred = iterations * sizeof(float) * elements * threads / (double)1e9;
     bw = 1000 * gbTransferred / (double)time_diff_ms;
+    if (!shared) bw = bw * threads; // iteration count is divided by thread count if in thread private mode
     //printf("%f GB, %lu ms\n", gbTransferred, time_diff_ms);
 
     free(testThreads);
-    free(testArr);
+    free(testArr); // should be null in not-shared (private) mode
+
+    if (!shared) {
+        for (uint64_t i = 0; i < threads; i++) {
+            free(threadData[i].arr);
+        }
+    }
+
+    free(threadData);
     return bw;
 }
 
