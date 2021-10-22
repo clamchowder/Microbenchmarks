f7970fb01ca7c316e3b6bc22d403c58def6955d7
clamchowder <lamchester@gmail.com>
2021-10-21 21:40:22 -0700

    avx512 for windows version of membw test

--- MemoryBandwidth/MemoryBandwidth/MemoryBandwidthFunctions.asm
+++ MemoryBandwidth/MemoryBandwidth/MemoryBandwidthFunctions.asm
@@ -3,6 +3,7 @@ section .text
 bits 64
 
 global avx_asm_read
+global avx512_asm_read
 
 ; rcx = float ptr to arr, rdx = fp32 elements in arr, r8 = iterations, r9 = start index
 ; return something in xmm0
@@ -49,6 +50,47 @@ asm_avx_test_iteration_count:
   jnz avx_asm_read_pass_loop ; skip iteration decrement if we're not back to start
   dec r8
   jnz avx_asm_read_pass_loop
+  pop r14
+  pop r15
+  pop rbx
+  pop rdi
+  pop rsi
+  ret
+
+avx512_asm_read:
+  push rsi
+  push rdi
+  push rbx
+  push r15
+  push r14
+  mov r15, 256 ; load in blocks of 256 bytes
+  sub rdx, 128 ; last iteration: rsi == rdx. rsi > rdx = break
+  mov rsi, r9  ; assume we're passed in an aligned start location O.o
+  xor rbx, rbx
+  lea rdi, [rcx + rsi * 4]
+  mov r14, rdi
+avx512_asm_read_pass_loop:
+  vmovaps zmm0, [rdi]
+  vmovaps zmm1, [rdi + 64]
+  vmovaps zmm2, [rdi + 128]
+  vmovaps zmm3, [rdi + 192]
+  add rsi, 64
+  add rdi, r15
+  vmovaps zmm0, [rdi]
+  vmovaps zmm1, [rdi + 64]
+  vmovaps zmm2, [rdi + 128]
+  vmovaps zmm3, [rdi + 192]
+  add rsi, 64
+  add rdi, r15
+  cmp rdx, rsi
+  jge asm_avx512_test_iteration_count
+  mov rsi, rbx
+  lea rdi, [rcx + rsi * 4]  ; back to start
+asm_avx512_test_iteration_count:
+  cmp r9, rsi
+  jnz avx512_asm_read_pass_loop ; skip iteration decrement if we're not back to start
+  dec r8
+  jnz avx512_asm_read_pass_loop
   pop r14
   pop r15
   pop rbx
