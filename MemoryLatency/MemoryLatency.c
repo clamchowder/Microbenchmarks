1655a3a204f3f145fc797d8ccc734d57502b22ac
clamchowder <lamchester@gmail.com>
2022-11-11 06:37:54 +0000

    test 128-bit stlf

--- MemoryLatency/MemoryLatency.c
+++ MemoryLatency/MemoryLatency.c
@@ -41,6 +41,7 @@ extern uint32_t latencytest(uint64_t iterations, uint64_t *arr);
 extern void matchedstlftest(uint64_t iterations, char *arr);
 extern void stlftest(uint64_t iterations, char *arr);
 extern void stlftest32(uint64_t iterations, char *arr);
+extern void stlftest128(uint64_t iterations, char *arr);
 void (*stlfFunc)(uint64_t, char *) = stlftest;
 #else
 #define UNKNOWN_ARCH 1
@@ -94,6 +95,10 @@ int main(int argc, char* argv[]) {
                     stlf = 1;
                     stlfFunc = matchedstlftest;
                     fprintf(stderr, "Running store to load forwarding test, with matched load/store sizes\n");
+                } else if (strncmp(testType, "128_stlf", 4) == 0) {
+                    stlf = 1;
+                    stlfFunc = stlftest128;
+                    fprintf(stderr, "Running store to load forwarding test, with 128-bit store, 64-bit load\n");
                 }
 		#ifndef BITS_32
                 else if (strncmp(testType, "dword_stlf", 9) == 0) {
